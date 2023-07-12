63a3dfcbe6e3858fe76bf13e5ad9631d1617b9c6
raduanakhawla <raduanakhawla468@gmail.com>
2023-07-12 20:08:31 +0600

    search payment

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const SSLCommerzPayment = require("sslcommerz");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 8000;
-
+const { v4: uuidv4 } = require("uuid");
 // middleware
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wjlgu.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
 async function verifyToken(req, res, next) {
   if (req.headers?.authorization?.startsWith("Bearer ")) {
     const token = req.headers.authorization.split(" ")[1];
@@ -41,7 +44,126 @@ async function run() {
     const ordersCollection = client
       .db("constructionDbUser")
       .collection("orders");
+    const sslOrderCollection = client
+      .db("constructionDbUser")
+      .collection("sslOrders");
+    // Initialize payment
+    app.post("/init", async (req, res) => {
+      console.log("hitting");
+      const productInfo = {
+        total_amount: req.body.total_amount,
+        currency: "BDT",
+        tran_id: uuidv4(),
+        success_url: "http://localhost:5000/success",
+        fail_url: "http://localhost:5000/failure",
+        cancel_url: "http://localhost:5000/cancel",
+        ipn_url: "http://localhost:5000/ipn",
+        paymentStatus: "pending",
+        shipping_method: "Courier",
+        product_name: req.body.product_name,
+        product_category: "Electronic",
+        product_profile: req.body.product_profile,
+        product_image: req.body.product_image,
+        cus_name: req.body.cus_name,
+        cus_email: req.body.cus_email,
+        cus_add1: "Dhaka",
+        cus_add2: "Dhaka",
+        cus_city: "Dhaka",
+        cus_state: "Dhaka",
+        cus_postcode: "1000",
+        cus_country: "Bangladesh",
+        cus_phone: "01711111111",
+        cus_fax: "01711111111",
+        ship_name: req.body.cus_name,
+        ship_add1: "Dhaka",
+        ship_add2: "Dhaka",
+        ship_city: "Dhaka",
+        ship_state: "Dhaka",
+        ship_postcode: 1000,
+        ship_country: "Bangladesh",
+        multi_card_name: "mastercard",
+        value_a: "ref001_A",
+        value_b: "ref002_B",
+        value_c: "ref003_C",
+        value_d: "ref004_D",
+      };
+      // Insert order info
+      const result = await sslOrderCollection.insertOne(productInfo);
 
+      const sslcommer = new SSLCommerzPayment(
+        process.env.STORE_ID,
+        process.env.STORE_PASSWORD,
+        false
+      ); //true for live default false for sandbox
+      sslcommer.init(productInfo).then((data) => {
+        //process the response that got from sslcommerz
+        //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+        const info = { ...productInfo, ...data };
+        //console.log(info.GatewayPageURL);
+        if (info.GatewayPageURL) {
+          res.json(info.GatewayPageURL);
+        } else {
+          return res.status(400).json({
+            message: "SSL session was not successful",
+          });
+        }
+      });
+    });
+    app.post("/success", async (req, res) => {
+      const result = await sslOrderCollection.updateOne(
+        { tran_id: req.body.tran_id },
+        {
+          $set: {
+            val_id: req.body.val_id,
+          },
+        }
+      );
+
+      res.redirect(`http://localhost:3000/success/${req.body.tran_id}`);
+    });
+    app.post("/failure", async (req, res) => {
+      const result = await sslOrderCollection.deleteOne({
+        tran_id: req.body.tran_id,
+      });
+
+      res.redirect(`http://localhost:3000`);
+    });
+    app.post("/cancel", async (req, res) => {
+      const result = await sslOrderCollection.deleteOne({
+        tran_id: req.body.tran_id,
+      });
+
+      res.redirect(`http://localhost:3000`);
+    });
+    app.post("/ipn", (req, res) => {
+      console.log(req.body);
+      res.send(req.body);
+    });
+    app.post("/validate", async (req, res) => {
+      const result = await sslOrderCollection.findOne({
+        tran_id: req.body.tran_id,
+      });
+
+      if (result.val_id === req.body.val_id) {
+        const update = await sslOrderCollection.updateOne(
+          { tran_id: req.body.tran_id },
+          {
+            $set: {
+              paymentStatus: "paymentComplete",
+            },
+          }
+        );
+        console.log(update);
+        res.send(update.modifiedCount > 0);
+      } else {
+        res.send("Chor detected");
+      }
+    });
+    app.get("/orders/:tran_id", async (req, res) => {
+      const id = req.params.tran_id;
+      const result = await sslOrderCollection.findOne({ tran_id: id });
+      res.json(result);
+    });
     // get all services
     app.get("/homeDesign", async (req, res) => {
       const result = await homeDesignCollection.find({}).toArray();
@@ -62,7 +184,7 @@ async function run() {
     app.get("/singleService/:id", async (req, res) => {
       console.log(req.params.id);
       const result = await apartmentBookingCollection
-        .find({ _id: ObjectId(req.params.id) })
+        .find({ _id: new ObjectId(req.params.id) })
         .toArray();
       res.json(result[0]);
     });
@@ -162,7 +284,7 @@ async function run() {
     //order delete
     app.delete("/deleteOrder/:id", async (req, res) => {
       const result = await ordersCollection.deleteOne({
-        _id: ObjectId(req.params.id),
+        _id: new ObjectId(req.params.id),
       });
       // console.log(result);
       res.json(result);
@@ -176,7 +298,7 @@ async function run() {
 
     // status update
     app.put("/statusUpdate/:id", async (req, res) => {
-      const filter = { _id: ObjectId(req.params.id) };
+      const filter = { _id: new ObjectId(req.params.id) };
       console.log(req.params.id);
       const result = await ordersCollection.updateOne(filter, {
         $set: {
